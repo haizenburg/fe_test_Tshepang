5601c458b340130f1868617aee9ab34827034135
tshepang <tshepangmoima87@gmail.com>
2024-08-11 23:43:23 +0200

    init commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import Sidebar from "../components/Sidebar";
+// import RightSidebar from "../components/RightSidebar";
+import Header from "../components/Header";
+
+export const metadata: Metadata = {
+  title: "Lunkgem",
+  description: "Discover and enjoy music"
+};
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="flex bg-white">
+        <Sidebar />
+        <div className="flex flex-col flex-grow">
+          <Header />
+          <main className="flex-grow p-8 overflow-y-auto">{children}</main>
+        </div>
+        {/* <RightSidebar /> */}
+      </body>
+    </html>
+  );
+}
